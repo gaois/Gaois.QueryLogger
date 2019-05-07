cf2088988d98aa342a80b5948de5f82f7a2ca771
Ronan Doherty <ronan.odochartaigh@gmail.com>
2019-05-07 18:30:05 +0100

    Failing SqlLogStore tests

---
+++ tests/Gaois.QueryLogger.Tests.AspNetCore/SqlLogStoreTests.cs
@@ -0,0 +1,50 @@
+﻿using Gaois.QueryLogger.AspNetCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Gaois.QueryLogger.Tests.AspNetCore
+{
+    public class SqlLogStoreTests
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public SqlLogStoreTests(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Fact]
+        public void Enqueue()
+        {
+            var configuration = TestHelper.GetConfiguration("appsettings.json");
+            var serviceProvider = new ServiceCollection()
+                .Configure<QueryLoggerSettings>(configuration.GetSection("QueryLogger"))
+                .AddOptions()
+                .AddTransient<IAlertService, EmailAlertService>()
+                .BuildServiceProvider();
+
+            var alertService = serviceProvider.GetService<IAlertService>();
+            var settings = serviceProvider.GetService<IOptionsMonitor<QueryLoggerSettings>>();
+
+            _outputHelper.WriteLine(settings.CurrentValue.ApplicationName);
+
+            var store = new SqlLogStore(settings, alertService);
+
+            // Add single query
+            var query = new Query()
+            {
+
+            };
+
+            // Test queue size
+            store.Enqueue(new[] { query });
+            Assert.Single(store.LogQueue);
+
+            // Test query value
+            var takeQuery = store.LogQueue.Take();
+            Assert.Equal("RecordsApp", takeQuery.ApplicationName);
+        }
+    }
+}
---
+++ tests/Gaois.QueryLogger.Tests.AspNetCore/TestHelper.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+using System.Reflection;
+
+namespace Gaois.QueryLogger.Tests.AspNetCore
+{
+    internal class TestHelper
+    {
+        public static string BasePath => Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+
+        public static string SettingsBasePath => Path.Combine(BasePath, "Settings");
+
+        public static IConfigurationRoot GetConfiguration(string jsonFileName)
+        {
+            return new ConfigurationBuilder()
+                .SetBasePath(SettingsBasePath)
+                .AddJsonFile(jsonFileName, optional: true)
+                .Build();
+        }
+
+        public static QueryLoggerSettings GetQueryLoggerConfiguration(string jsonFileName)
+        {
+            var configuration = new QueryLoggerSettings();
+            var configRoot = GetConfiguration(jsonFileName);
+            configRoot.GetSection("QueryLogger").Bind(configuration);
+            return configuration;
+        }
+    }
+}
